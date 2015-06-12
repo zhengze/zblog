329492e7ad0eb4dca72fa4f6876b2e1ade29d7e1
zhengze <zhang_zhze@sina.com>
2015-06-13 07:54:45 +0800

    增加article

--- zblog/urls.py
+++ zblog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^$', views.IndexView.as_view(), name='index'),    
+    url(r'^article$', views.ArticleView.as_view(), name='article'),    
     url('^hello/$', views.hello),     
 )
