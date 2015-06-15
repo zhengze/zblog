b41641c46a7d565e6413d78e7daa8f66f2823c54
zhengze <zhang_zhze@sina.com>
2015-06-15 17:43:43 +0800

    去掉hello的url

--- zblog/urls.py
+++ zblog/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = patterns('',
     url(r'^$', views.IndexView.as_view(), name='index'),    
     url(r'^article$', views.ArticleView.as_view(), name='article'),    
-    url('^hello/$', views.hello),     
+    url(r'^picture$', views.PictureView.as_view(), name='picture'),    
+    url(r'^music$', views.MusicView.as_view(), name='music'),    
+    url(r'^about$', views.AboutView.as_view(), name='about'),    
 )
