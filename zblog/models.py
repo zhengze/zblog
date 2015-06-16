f8122741b57a661fca5e5d119dc523560ef8f1ab
zhengze <zhang_zhze@sina.com>
2015-06-16 15:25:48 +0800

    文章内容CharField --> TextField

--- zblog/models.py
+++ zblog/models.py
@@ -1,11 +1,24 @@
 from django.db import models
 
-class Article(models.Model):
-	title = models.CharField(max_length=50)
-	content = models.CharField(max_length=255)
-
 class Classify(models.Model):
-	name = models.CharField(max_length=20)
-	def __unicode__(self):
-		return self.name
+    name = models.CharField(max_length=20)
+    def __unicode__(self):
+        return self.name
 
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+    def __unicode__(self):
+        return self.name
+
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    created_time = models.DateTimeField()
+    classify = models.ForeignKey(Classify)
+    tag = models.ForeignKey(Tag)
+    def __unicode__(self):
+        return self.title
+    class Meta:
+        ordering = ['-created_time']
+        
+    
