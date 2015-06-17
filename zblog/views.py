49e4512e98aa19c67cd039e466e17ea6b1d9fe5c
zhengze <zhang_zhze@sina.com>
2015-06-17 15:46:20 +0800

    add ArticleDetailView

--- zblog/views.py
+++ zblog/views.py
@@ -2,31 +2,43 @@
 
 from django.shortcuts import get_object_or_404, render
 from django.core.urlresolvers import reverse
-from django.views import generic
+from django.views.generic import ListView
 from .models import Article, Classify
 
-class IndexView(generic.ListView):
+class IndexView(ListView):
     model = Article
-    context_object_name = 'article'
+    context_object_name = 'article_list'
     template_name = 'zblog/index.html'
     paginate_by = 10
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         context['classifies'] = Classify.objects.all()
         return context
-class ArticleView(generic.ListView):
+
+class ArticleView(ListView):
     model = Article
     template_name = 'zblog/article.html'
 
-class PictureView(generic.ListView):
+class ArticleDetailView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'zblog/article_detail.html'
+    def get_context_data(self, **kwargs):
+        context = super(ArticleDetailView, self).get_context_data(**kwargs)
+        context['classifies'] = Classify.objects.all()
+        return context
+    def get_queryset(self):
+        return Article.objects.filter(pk=self.args[0])
+
+class PictureView(ListView):
     model = Article
     template_name = 'zblog/picture.html'
 
-class MusicView(generic.ListView):
+class MusicView(ListView):
     model = Article
     template_name = 'zblog/music.html'
 
-class AboutView(generic.ListView):
+class AboutView(ListView):
     model = Article
     template_name = 'zblog/aboutme.html'
 
