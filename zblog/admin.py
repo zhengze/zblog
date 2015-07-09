6da3c1a7b0e1e4e31392cbb4e4a88fe15ef84615
zhengze <zhang_zhze@sina.com>
2015-07-09 21:21:09 +0800

    register Image and ImageClassify

--- zblog/admin.py
+++ zblog/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from .models import * 
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content', 'created_time', 'classify', 'tag')
+    list_display = ('title', 'content', 'created_time', 'classify')
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Classify)
 admin.site.register(Tag)
+admin.site.register(Image)
+admin.site.register(ImageClassify)
