55a5abfc155f0699584b2be902fd914810472e0c
zhengze <zhang_zhze@sina.com>
2015-07-10 02:23:07 +0800

    ImageView --> PhotoView

--- zblog/urls.py
+++ zblog/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
     url(r'^article$', views.ArticleView.as_view(), name='article'),    
     url(r'^article/detail/(\d+)/$', views.ArticleDetailView.as_view(), name='article_detail'),  
     url(r'^article/classification/(\d+)/$', views.ArticleClassificationView.as_view(), name='article_classification'),    
-    url(r'^picture$', views.PictureView.as_view(), name='picture'),    
+    url(r'^photos$', views.PhotoView.as_view(), name='photos'),    
     url(r'^music$', views.MusicView.as_view(), name='music'),    
     url(r'^about$', views.AboutView.as_view(), name='about'),    
 )
