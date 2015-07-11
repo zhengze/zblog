48587d0c7a5906bd95c2c26db92365bf0843ddee
zhengze <zhang_zhze@sina.com>
2015-07-11 17:28:11 +0800

    add Music

--- zblog/models.py
+++ zblog/models.py
@@ -2,23 +2,24 @@ from django.db import models
 
 class Classify(models.Model):
     name = models.CharField(max_length=20)
+
     def __unicode__(self):
         return self.name
 
 class Tag(models.Model):
     name = models.CharField(max_length=20)
+
     def __unicode__(self):
         return self.name
     
 class Article(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
-    created_time = models.DateTimeField()
+    created_time = models.DateTimeField(auto_now=True)
     classify = models.ForeignKey(Classify)
     tag = models.ForeignKey(Tag)
     hits = models.IntegerField(max_length=1, default=0)
 
-    
     class Meta:
         ordering = ['-created_time']
     def __unicode__(self):
@@ -27,12 +28,27 @@ class Article(models.Model):
 class Album(models.Model):
     name = models.CharField(max_length=50)
     cover = models.ImageField(upload_to='images/covers/%Y/%m/%d', null=True, blank=True)
+
     def __unicode__(self):
         return self.name    
 
 class Photo(models.Model):
     photo = models.ImageField(upload_to='images/%Y/%m/%d')
     description = models.CharField(max_length=255, null=True)
+    created_time = models.DateTimeField(auto_now=True, null=True)
     album = models.ForeignKey(Album)
+
+    class Meta:
+        ordering = ['-created_time']
     def __unicode__(self):
         return self.description
+
+class Music(models.Model):
+    title =  models.CharField(max_length=50)
+    mp3 = models.FileField(upload_to='music/%Y/%m/%d')
+    created_time = models.DateTimeField(auto_now=True, null=True)
+
+    class Meta:
+        ordering = ['-created_time']
+    def __unicode__(self):
+        return self.title
