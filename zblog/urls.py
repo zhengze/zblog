8c79b3d5fb0fe9668b2f833fe81b5fb1fa1e035b
zhengze <zhang_zhze@sina.com>
2015-07-11 17:27:30 +0800

    add original_photo

--- zblog/urls.py
+++ zblog/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     url(r'^article/classification/(\d+)/$', views.ArticleClassificationView.as_view(), name='article_classification'),    
     url(r'^album$', views.AlbumView.as_view(), name='album'),    
     url(r'^photo/(\d+)/$', views.PhotoView.as_view(), name='photo'),    
+    url(r'^original_photo/(\d+)/(\d+)/$', views.OriginalPhotoView.as_view(), name='original_photo'),    
     url(r'^music$', views.MusicView.as_view(), name='music'),    
     url(r'^about$', views.AboutView.as_view(), name='about'),    
 )
