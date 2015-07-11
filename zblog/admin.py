f119f7690b3fd375ae51263f9a5920bd70ed0017
zhengze <zhang_zhze@sina.com>
2015-07-11 17:29:03 +0800

    内容显示处理

--- zblog/admin.py
+++ zblog/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
 from .models import * 
 
+@admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content', 'created_time', 'classify')
+    list_display = ('title', 'content', 'created_time', 'classify', 'hits')
+    exclude = ('hits', )
+    list_filter = ('created_time', 'classify')
+    search_fields = ['title', 'classify']
 
-admin.site.register(Article, ArticleAdmin)
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('photo', 'album', 'description', 'created_time')
+    list_filter = ('created_time', 'album')
+    search_fields = ['album']
+
+@admin.register(Music)
+class MusicAdmin(admin.ModelAdmin):
+    list_display = ('title', 'mp3', 'created_time')
+    list_filter = ('created_time', )
+
 admin.site.register(Classify)
 admin.site.register(Tag)
-admin.site.register(Photo)
 admin.site.register(Album)
