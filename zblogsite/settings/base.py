437ac348f81aa6ede08ac0eab2c052b25e30f5cf
zhengze <zhang_zhze@sina.com>
2015-07-13 11:26:05 +0800

    comment staticfiles_dirs

--- zblogsite/settings/base.py
+++ zblogsite/settings/base.py
@@ -50,20 +50,20 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static').replace('\\', '/')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static').replace('\\', '/')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 
 # Additional locations of static files
-STATICFILES_DIRS = (
+#STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 
-    os.path.join(BASE_DIR, 'static').replace('\\', '/'),
-)
+#    os.path.join(BASE_DIR, 'static').replace('\\', '/'),
+#)
 
 # List of finder classes that know how to find static files in
 # various locations.
