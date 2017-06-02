8ca141ab974b18038d0b1435dea75818e73b8922
zhanghai <hai.zhang@fun-sensor.com>
2017-06-02 19:08:33 +0800

    django1.11写法

--- zblogsite/urls.py
+++ zblogsite/urls.py
@@ -1,27 +1,30 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
+from django.contrib import admin
 from django.conf.urls.static import static
-from zblogsite import settings
+from django.views.static import serve
+import zblog
+from zblogsite.settings.dev import MEDIA_ROOT
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     url(r'^', include('zblog.urls')),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
-)
+    url(r'^admin/', admin.site.urls),
+]
 
-urlpatterns += patterns('',
+urlpatterns += [
     #url(r'^static/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.base.STATIC_ROOT },name='static'),
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.base.MEDIA_ROOT },name='media'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': MEDIA_ROOT },name='media'),
     #url(r'js/(?P<path>[\w\.\-]+\.js)$', 'django.views.static.serve',{'document_root':os.path.join(os.path.dirname(__file__),"./static/js")},name='js'),
     #url(r'css/(?P<path>[\w\.\-]+\.css)$', 'django.views.static.serve',{'document_root':os.path.join(os.path.dirname(__file__),"./static/css")},name='css'),
     #url(r'images/(?P<path>[\w+.\-]+\.*)$', 'django.views.static.serve',{'document_root':os.path.join(os.path.dirname(__file__),"./static/images/")},name='images'),
     #url(r'fonts/(?P<path>[\w+.\-]+\.*)$', 'django.views.static.serve',{'document_root':os.path.join(os.path.dirname(__file__),"./static/fonts/")},name='fonts'),
-)
+]
 #urlpatterns+static(settings.base.STATIC_URL,document_root=settings.base.STATIC_ROOT)
 #urlpatterns+static(settings.base.MEDIA_URL,document_root=settings.base.MEDIA_ROOT)
