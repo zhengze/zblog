3746ab3c84ede313b2fa6ccf00406220e252424d
zhanghai <hai.zhang@fun-sensor.com>
2017-06-02 19:08:38 +0800

    django1.11写法

--- zblog/urls.py
+++ zblog/urls.py
@@ -1,14 +1,14 @@
-from django.conf.urls import patterns, include, url
-from . import views
+from django.conf.urls import include, url
+from zblog.views import  IndexView, ArticleView, ArticleDetailView, ArticleClassificationView, AlbumView, PhotoView, OriginalPhotoView,MusicView, AboutView
 
-urlpatterns = patterns('',
-    url(r'^$', views.IndexView.as_view(), name='index'),    
-    url(r'^article$', views.ArticleView.as_view(), name='article'),    
-    url(r'^article/detail/(\d+)/$', views.ArticleDetailView.as_view(), name='article_detail'),  
-    url(r'^article/classification/(\d+)/$', views.ArticleClassificationView.as_view(), name='article_classification'),    
-    url(r'^album$', views.AlbumView.as_view(), name='album'),    
-    url(r'^photo/(\d+)/$', views.PhotoView.as_view(), name='photo'),    
-    url(r'^original_photo/(\d+)/(\d+)/$', views.OriginalPhotoView.as_view(), name='original_photo'),    
-    url(r'^music$', views.MusicView.as_view(), name='music'),    
-    url(r'^about$', views.AboutView.as_view(), name='about'),    
-)
+urlpatterns = [
+    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^article$', ArticleView.as_view(), name='article'),
+    url(r'^article/detail/(\d+)/$', ArticleDetailView.as_view(), name='article_detail'),
+    url(r'^article/classification/(\d+)/$', ArticleClassificationView.as_view(), name='article_classification'),
+    url(r'^album$', AlbumView.as_view(), name='album'),
+    url(r'^photo/(\d+)/$', PhotoView.as_view(), name='photo'),
+    url(r'^original_photo/(\d+)/(\d+)/$', OriginalPhotoView.as_view(), name='original_photo'),
+    url(r'^music$', MusicView.as_view(), name='music'),
+    url(r'^about$', AboutView.as_view(), name='about'),
+]
