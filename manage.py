65d8f17750c9d979a06a89137e83b1257850e9c2
zhanghai <hai.zhang@fun-sensor.com>
2017-06-06 17:16:30 +0800

    环境配置

--- manage.py
+++ manage.py
@@ -3,12 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    ENV_DEV = True
-    if ENV_DEV:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zblogsite.settings")
-    else:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zblogsite.settings")
-
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zblogsite.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
