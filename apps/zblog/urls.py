aef4821ee131b0bf8cdb7d9e1044bfb47e0c4cc1
zhanghai <hai.zhang@fun-sensor.com>
2017-07-26 23:52:12 +0800

    为id取名

--- apps/zblog/urls.py
+++ apps/zblog/urls.py
@@ -1,16 +1,30 @@
-from django.conf.urls import url
+# coding:utf-8
 
-from apps.zblog.views import IndexView, ArticleView, ArticleDetailView, ArticleClassificationView, AlbumView, PhotoView, \
-    OriginalPhotoView, MusicView, AboutView
+
+from django.conf.urls import url
+from apps.zblog.views import (
+    IndexView,
+    ArticleView,
+    ArticleDetailView,
+    ArticleClassificationView,
+    AlbumView,
+    PhotoView,
+    OriginalPhotoView,
+    MusicView,
+    AboutView
+)
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
     url(r'^article$', ArticleView.as_view(), name='article'),
-    url(r'^article/detail/(\d+)/$', ArticleDetailView.as_view(), name='article_detail'),
-    url(r'^article/classification/(\d+)/$', ArticleClassificationView.as_view(), name='article_classification'),
+    url(r'^article/detail/(?P<article_id>\d+)/$', ArticleDetailView.as_view(), name='article_detail'),
+    url(r'^article/classification/(?P<classification_id>\d+)/$',
+        ArticleClassificationView.as_view(),
+        name='article_classification'
+    ),
     url(r'^album$', AlbumView.as_view(), name='album'),
-    url(r'^photo/(\d+)/$', PhotoView.as_view(), name='photo'),
-    url(r'^original_photo/(\d+)/(\d+)/$', OriginalPhotoView.as_view(), name='original_photo'),
+    url(r'^photo/(?P<album_id>\d+)/$', PhotoView.as_view(), name='photo'),
+    url(r'^original_photo/(?P<album_id>\d+)/(?P<photo_id>\d+)/$', OriginalPhotoView.as_view(), name='original_photo'),
     url(r'^music$', MusicView.as_view(), name='music'),
     url(r'^about$', AboutView.as_view(), name='about'),
 ]
