d4bc595f30fa71d6e059ac584800328b944faf3f
zhanghai <hai.zhang@fun-sensor.com>
2017-08-02 14:48:25 +0800

    AlbumAdmin, ClassifyAdmin, TagAdmin

--- apps/zblog/admin.py
+++ apps/zblog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import * 
 
+
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'content', 'created_time', 'classify', 'hits')
@@ -8,17 +9,32 @@ class ArticleAdmin(admin.ModelAdmin):
     list_filter = ('created_time', 'classify')
     search_fields = ['title', 'classify']
 
+
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
     list_display = ('photo', 'album', 'description', 'created_time')
     list_filter = ('created_time', 'album')
     search_fields = ['album']
 
+
 @admin.register(Music)
 class MusicAdmin(admin.ModelAdmin):
     list_display = ('title', 'mp3', 'created_time')
     list_filter = ('created_time', )
 
-admin.site.register(Classify)
-admin.site.register(Tag)
-admin.site.register(Album)
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('name', 'cover')
+
+
+@admin.register(Classify)
+class ClassifyAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
