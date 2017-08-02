e9fa6da24f917e825366e42add6635aa8e8b54ca
zhanghai <hai.zhang@fun-sensor.com>
2017-08-02 14:49:57 +0800

    models优化

--- apps/zblog/models.py
+++ apps/zblog/models.py
@@ -4,27 +4,36 @@ from django.db import models
 class Classify(models.Model):
     name = models.CharField(max_length=20)
 
+    class Meta:
+        verbose_name_plural = verbose_name = u'分类'
+
     def __str__(self):
         return self.name
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, verbose_name=u"标签")
+
+    class Meta:
+        verbose_name_plural = verbose_name = u'标签'
 
     def __str__(self):
         return self.name
 
 
 class Article(models.Model):
-    title = models.CharField(max_length=50)
-    content = models.TextField()
-    created_time = models.DateTimeField(auto_now=True)
-    classify = models.ForeignKey(Classify)
-    tag = models.ForeignKey(Tag)
-    hits = models.IntegerField(default=0)
+    title = models.CharField(max_length=50, verbose_name=u"标题")
+    content = models.TextField(verbose_name=u"正文", blank=True)
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name=u"创建时间")
+    updated_time = models.DateTimeField(auto_now=True, verbose_name=u"修改时间")
+    classify = models.ForeignKey(Classify, verbose_name=u"分类", on_delete=models.CASCADE)
+    #tag = models.ManyToManyField(Tag, related_name="article")
+    tags = models.ManyToManyField(Tag, related_name="articles", blank=True, verbose_name=u"标签")
+    hits = models.IntegerField(default=0, verbose_name=u"浏览量")
 
     class Meta:
         ordering = ['-created_time']
+        verbose_name_plural = verbose_name = u'文章'
 
     def __str__(self):
         return self.title
@@ -32,7 +41,11 @@ class Article(models.Model):
 
 class Album(models.Model):
     name = models.CharField(max_length=50)
-    cover = models.ImageField(upload_to='images/covers/%Y/%m/%d', null=True, blank=True)
+    cover = models.ImageField(upload_to='images/covers/%Y/%m/%d', blank=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = verbose_name = u'封面'
 
     def __str__(self):
         return self.name    
@@ -41,11 +54,12 @@ class Album(models.Model):
 class Photo(models.Model):
     photo = models.ImageField(upload_to='images/%Y/%m/%d')
     description = models.CharField(max_length=255, null=True)
-    created_time = models.DateTimeField(auto_now=True, null=True)
+    created_time = models.DateTimeField(auto_now_add=True)
     album = models.ForeignKey(Album)
 
     class Meta:
         ordering = ['-created_time']
+        verbose_name_plural = verbose_name = u'相册'
 
     def __str__(self):
         return self.description
@@ -54,10 +68,11 @@ class Photo(models.Model):
 class Music(models.Model):
     title =  models.CharField(max_length=50)
     mp3 = models.FileField(upload_to='music/%Y/%m/%d')
-    created_time = models.DateTimeField(auto_now=True, null=True)
+    created_time = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['-created_time']
+        verbose_name_plural = verbose_name = u'音乐'
 
     def __str__(self):
         return self.title
