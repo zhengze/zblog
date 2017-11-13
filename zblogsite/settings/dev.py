9b51ec0d8c3d9c1d2bac03f2048dd97b4e7ba616
zhengze <zhanghhd@163.com>
2017-11-13 02:17:23 -0800

    HOST

--- zblogsite/settings/dev.py
+++ zblogsite/settings/dev.py
@@ -9,7 +9,8 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'root',
         'PASSWORD': '1234',
-        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        #'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': 'db',                      # docker mysql.
         'PORT': '3306',                      # Set to empty string for default.
     }
 }
