480eacd661ef7b6a8526442d73eda644c5ab7014
zhengze <zhanghhd@163.com>
2017-11-19 06:27:19 -0800

    loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 var path = require("path")
+var webpack = require("webpack")
 var BundleTracker = require('webpack-bundle-tracker');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   context: __dirname,
-  entry: "./zblogsite/static/js/src/index", //entry文件的位置
+  entry: {
+    base: "./apps/zblog/webpack_js/base.js",
+  }, //entry文件的位置
   output: {
-    path: path.resolve('./zblogsite/static/js/dist'), //和settings.py里的WEBPACK_LOADER的设置对应
+    path: path.resolve('./zblogsite/bundles'), //和settings.py里的WEBPACK_LOADER的设置对应
     filename: "[name].dev.js"
   },
   module: { //使用babel loader
@@ -17,11 +21,13 @@ module.exports = {
         query: {
           presets: ['es2015']
         }
-      }
+      },
+      {test: /\.css$/, loader: 'style-loader!css-loader'},
+      {test: /\.jpg$/, loader: 'url-loader?limit=100000'}
     ]
   },
   plugins: [
-    new BundleTracker({filename: './webpack-stats.json'})
+    new BundleTracker({filename: './zblogsite/webpack-stats.json'})
   ],
-  devtool: "source-map"
+  devtool: "eval-source-map"
 };
