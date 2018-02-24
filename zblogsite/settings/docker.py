c7c02febb43eb2466484f5c99d6dcc2d60e67e09
600408 <600408@syswin.com>
2018-02-24 18:52:56 +0800

    add docker.py

---
+++ zblogsite/settings/docker.py
@@ -0,0 +1,19 @@
+from .base import *
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'zblog',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'root',
+        'PASSWORD': '1234',
+        #'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': 'db',                      # docker mysql.
+        'PORT': '3306',                      # Set to empty string for default.
+        'OPTIONS': {
+            'init_command': "Set sql_mode='STRICT_TRANS_TABLES'"
+        }
+    }
+}
