402a793492e69321938ba6d96ffd57253af76754
600408 <600408@syswin.com>
2018-02-24 18:53:31 +0800

    rest_framework

--- zblogsite/settings/dev.py
+++ zblogsite/settings/dev.py
@@ -9,8 +9,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'root',
         'PASSWORD': '1234',
-        #'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'HOST': 'db',                      # docker mysql.
+        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',                      # Set to empty string for default.
         'OPTIONS': {
             'init_command': "Set sql_mode='STRICT_TRANS_TABLES'"
