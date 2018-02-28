557ee3694dfcb3d3c228b1f064ea87615acdcac7
600408 <600408@syswin.com>
2018-02-28 19:05:41 +0800

    CategorySerializer, TagSerializer

--- apps/zblog/serializers.py
+++ apps/zblog/serializers.py
@@ -1,9 +1,21 @@
 
+from apps.zblog.models import Article, Category, Tag
 from rest_framework import serializers
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('name')
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('name')
+
 # Serializers define the API representation.
-class ArticleSerializer(serializers.HyperlinkedModelSerializer):
+class ArticleSerializer(serializers.ModelSerializer):
+    categorys = CategorySerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
     class Meta:
         model = Article
-        #fields = ('title', 'content', 'hits', 'created_time', 'updated_time', 'category', 'tags')
-        fields = ('title', 'content', 'hits', 'created_time', 'updated_time')
+        fields = ('title', 'content', 'hits', 'created_time', 'updated_time', 'categorys', 'tags')
