eaa39d6b7c400658390e2257e00834e7a97a1b64
zhengze <zhanghhd@163.com>
2018-02-27 18:55:02 -0800

    add api_view

---
+++ apps/zblog/api_view/views.py
@@ -0,0 +1,9 @@
+from zblog.models import Article
+from rest_framework import viewsets
+from apps.zblog.serializers import ArticleSerializer
+
+
+# ViewSets define the view behavior.
+class ArticleViewSet(viewsets.ModelViewSet):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
